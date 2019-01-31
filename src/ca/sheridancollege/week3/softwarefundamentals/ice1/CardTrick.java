e39cb32e8feabb8389e06e259a02b76daa8300ef
Michael-Nam-Tran-Sheridan <trmichae@sheridancollege.ca>
2019-01-30 20:53:42 -0500

    Added Student number to card.java and inserted random number generator for value and suit in forloop for masterhand. added a prompt asking users for their card's value and suit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,12 +5,15 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
- * @modifier Michael Nam Tran
+ * @modifier Michael Nam Tran 991363499
  */
 public class CardTrick {
     
@@ -21,13 +24,24 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random rand = new Random();
+            c.setValue(rand.nextInt(7));    //c.setValue(insert call to random number generator here)
+           c.setSuit(Card.SUITS[rand.nextInt(3)]); //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner cardNumber = new Scanner(System.in);
+            System.out.println("Choose your card's number?");
+                int number = cardNumber.nextInt();
+                
+        Scanner cardSuit = new Scanner(System.in);
+            System.out.println("What is your card's Suit?");
+                String suit = cardSuit.nextLine();
+        
+                System.out.println("Your Card is: " + number + " of " + suit);
         // and search magicHand here
+                
         //Then report the result here
+        }         
     }
     
-}

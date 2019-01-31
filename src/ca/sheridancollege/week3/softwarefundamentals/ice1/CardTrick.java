205a0431684eca1fb31cb7cbe2f8d22d387e4328
Michael-Nam-Tran-Sheridan <trmichae@sheridancollege.ca>
2019-01-31 13:57:27 -0500

    Moved for loop down because it was getting filled with null values and added if statement to compare user input with what Magichand has.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,39 +9,43 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
  * @author dancye
  * @modifier Michael Nam Tran 991363499
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            Random rand = new Random();
-            c.setValue(rand.nextInt(7));    //c.setValue(insert call to random number generator here)
-           c.setSuit(Card.SUITS[rand.nextInt(3)]); //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
+
+        Random rand = new Random();
+
         //insert code to ask the user for Card value and suit, create their card
         Scanner cardNumber = new Scanner(System.in);
-            System.out.println("Choose your card's number?");
-                int number = cardNumber.nextInt();
-                
+        System.out.println("Choose your card's number?");
+        int number = cardNumber.nextInt();
+
         Scanner cardSuit = new Scanner(System.in);
-            System.out.println("What is your card's Suit?");
-                String suit = cardSuit.nextLine();
-        
-                System.out.println("Your Card is: " + number + " of " + suit);
+        System.out.println("What is your card's Suit?, case sensitive");
+        String suit = cardSuit.nextLine();
+
+        System.out.println("Your Card is: " + number + " of " + suit);
         // and search magicHand here
-                
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+
+            c.setValue(rand.nextInt(7));    //c.setValue(insert call to random number generator here)
+            c.setSuit(Card.SUITS[rand.nextInt(3)]); //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            System.out.println(c.getValue() + " of " + c.getSuit());
+            if (number == c.getValue() && suit.equals(c.getSuit())) {
+                System.out.println("Card Matches");
+            } else {
+                System.out.println("Card Doesn't Match");
+            }
+        }
         //Then report the result here
-        }         
     }
-    
+}

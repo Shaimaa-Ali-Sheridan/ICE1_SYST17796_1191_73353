addece7342e791d5867ed83947ca2562555e8462
kapiljayswal <46981107+kapiljayswal@users.noreply.github.com>
2019-01-30 20:40:31 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+  inclass activity #1
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 

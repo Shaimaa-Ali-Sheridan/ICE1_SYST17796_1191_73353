ee1176c2959f84ee7900a6a0a07930b775e07127
range <range@10.48.62.73>
2019-02-06 16:45:54 -0500

    random function for Value and Suits are created and the user is asked to enter a value and a suit and then check the array if the user choice exist.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,22 +12,57 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * @author dancye
  * Date : Jan 30,2019
  */
+import java.lang.Math;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
         
+        Card[] magicHand = new Card[7];
+        Card c = new Card();
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(randomNumber());
+            //System.out.println(c.getValue());
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(randomString());
+            System.out.println(c.getValue()+ c.getSuit());
+        }
+        //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Please enter the number of your choice from 1-13!");
+        Scanner input = new Scanner(System.in);
+        int userN = input.nextInt();
+        System.out.print(userN);
+        
+        System.out.print("Please enter the Suit!");
+        String userS = input.nextLine();
+        System.out.print("1"+userS);
+        
+        for(int i=0; i<magicHand.length; i++ ){
+        // and search magicHand here
+            if(c.getValue()==userN && c.getSuit()==userS){
+                System.out.print("yes, the card matches!");
+                continue;
+            }
+            System.out.print("no, the card does not match!");
+        //Then report the result here
+        }
+    }
+    public static String randomString(){
+        int x = (int)(Math.random()*4);
+        if(x == 0)
+            return "Hearts";
+        else if(x == 1)
+            return "Diamonds";
+        else if(x == 2)
+            return "Spades";
+        else
+            return "Clubs";
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+     public static int randomNumber(){
+        return (int)(Math.random()*13+1);
     }
-    
 }

3ec195c36b7b0155f004fddc6f372ad1bfa44a42
ganeshwc <46981069+ganeshwc@users.noreply.github.com>
2019-02-13 15:54:21 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -28,6 +28,8 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+     System.out.println("hello, good day!!!!");
     }
+ 
     
 }
